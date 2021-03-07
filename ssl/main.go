8258096d858d562b6e20c8a54c7d3bc6ba9616a2
ShuwenHe <1201220707@pku.edu.cn>
2021-03-07 20:43:12 +0800

    feat: ssl & pk

---
+++ ssl/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"net"
+	"os"
+	"time"
+)
+
+func main() { //生成SSL证书和私钥
+	max := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, _ := rand.Int(rand.Reader, max)
+	subject := pkix.Name{
+		Organization:       []string{"书文教育"},
+		OrganizationalUnit: []string{"互联网教育"},
+		CommonName:         "学思题库",
+	}
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject:      subject,
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(365 * 24 * time.Hour),
+		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth}, //
+		IPAddresses:  []net.IP{net.ParseIP("127.0.0.1")},             // 证书设置成只能在IP地址为127上运行
+	}
+	pk, _ := rsa.GenerateKey(rand.Reader, 2048)
+	derBytes, _ := x509.CreateCertificate(rand.Reader, &template, &template, &pk.PublicKey, pk)
+	certOut, _ := os.Create("cert.pem")
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	certOut.Close()
+	keyOut, _ := os.Create("key.pem")
+	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(pk)})
+	keyOut.Close()
+}
